713d81f5737b4071301b41e2dbf0b324a2bdc768
Shrubhog <noah@danielgarcia.info>
2018-10-08 16:29:36 -0500

    add auto coffees

--- main.js
+++ main.js
@@ -14,13 +14,32 @@ function buyCoffeeMachines(){
           $("#coffeeMachines").html(coffeeMachines)
         }
         coffeeMachines = coffeeMachines + 1;
-    	   money = money - coffeeMachineCost;
+    	  money = money - coffeeMachineCost;
         $('#coffeeMachines').html(coffeeMachines);
         $('#money').html(money);
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,coffeeMachines));
+    var nextCost = Math.floor(100 * Math.pow(1.1,coffeeMachines));
 };
 
+var autoCoffees = 0;
+function buyAutoCoffee(){
+
+    var autoCoffeeCost = Math.floor(50 * Math.pow(1.1,autoCoffees));
+    if(money >= autoCoffeeCost && coffeeMachines > 1){
+        if (typeof $("#autoCoffees").html() == "undefined") {
+          $(".info").append("Auto Coffee Machines: <span id='autoCoffees'></span><br>")
+          $("#autoCoffees").html(autoCoffees)
+        }
+        autoCoffees += 1;
+    	  money = money - autoCoffeeCost;
+        coffeeMachines = coffeeMachines - 1;
+        $('#autoCoffees').html(autoCoffees);
+        $('coffeeMachines').html(coffeeMachines);
+        $('#money').html(money);
+    };
+    var nextCost = Math.floor(50 * Math.pow(1.1,coffeeMachines));
+};
 
 window.setInterval(function() {
+  getPaid(autoCoffees);
 }, 1000)
