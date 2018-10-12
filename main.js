5607115deb757253d210dbe3777a0a61d5fb783d
Shrubhog <noah@danielgarcia.info>
2018-10-12 18:27:17 -0500

    Added tooltip updater

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var money = 0;
 
+var tooltips = {
+  coffee: "Costs $-, allows you to make coffee. It's pretty good coffee.",
+  autoCoffee: "Costs $- and a coffee machine, gives an auto coffee machine and $1/s. Making coffee is pretty slow. Let's make the robots do it!"
+}
+
 function getPaid(num) {
   money += num;
   $("#money").html(money);
@@ -19,6 +24,9 @@ function buyCoffeeMachines(){
         $('#money').html(money);
     };
     var nextCost = Math.floor(100 * Math.pow(1.1,coffeeMachines));
+    var priceTooltip1 = tooltips.coffee.split("-");
+    var priceTooltip2 = priceTooltip1[0] + nextCost + priceTooltip1[1];
+    $("button#buyCoffeeMachine").attr("title", priceTooltip2)
 };
 
 var autoCoffees = 0;
@@ -32,13 +40,16 @@ function buyAutoCoffee() {
           $("#autoCoffees").html(autoCoffees)
         }
         autoCoffees += 1;
-    	money = money - autoCoffeeCost;
+    	  money = money - autoCoffeeCost;
         coffeeMachines = coffeeMachines - 1;
         $('#autoCoffees').html(autoCoffees);
         $('#coffeeMachines').html(coffeeMachines);
         $('#money').html(money);
     };
     var nextCost = Math.floor(50 * Math.pow(1.1,autoCoffees));
+    var priceTooltip1 = tooltips.autoCoffee.split("-");
+    var priceTooltip2 = priceTooltip1[0] + nextCost + priceTooltip1[1];
+    $("button#buyAutoCoffee").attr("title", priceTooltip2)
 };
 
 window.setInterval(function() {
